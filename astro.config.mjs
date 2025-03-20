3d7b628901929638c4f90e71ed42868bd3efa3d6
Jamel <strangerdante@gmail.com>
2025-03-19 23:04:15 -0500

    arreglo librerias

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,25 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), vue()]
+  integrations: [tailwind(), vue()],
+  vite: {
+    build: {
+      rollupOptions: {
+        external: [
+          "prosemirror-state",
+          "prosemirror-view",
+          "prosemirror-model",
+          "prosemirror-schema-basic",
+          "prosemirror-schema-list",
+          "prosemirror-keymap",
+          "prosemirror-commands",
+          "prosemirror-history",
+        ],
+      },
+    },
+  },
 });
